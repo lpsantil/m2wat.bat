46033f12f3b63ae7e2519f8a132e8f7c0a526d32
Louis P. Santillan <lpsantil@gmail.com>
2016-08-26 18:33:10 -0700

    Fixed comment redirection issue

--- m2wat.bat
+++ m2wat.bat
@@ -1,38 +1,38 @@
 @echo off
-REM -- m2wat.bat --
-REM -- Copyright (c) 2016, Louis P. Santillan <lpsantil@gmail.com>
-REM -- All rights reserved.
+:: -- m2wat.bat --
+:: -- Copyright (c) 2016, Louis P. Santillan <lpsantil@gmail.com>
+:: -- All rights reserved.
 
-REM -- Usage: m2wat.bat
+:: -- Usage: m2wat.bat
 
-REM ---------- CHECK ENVIRONMENT ----------
-REM -- Check for properly setup environment
+:: ---------- CHECK ENVIRONMENT ----------
+:: -- Check for properly setup environment
 IF "%MTCPCFG%"=="" goto MTCPCFGERR
 IF "%WATTCP.CFG%"=="" goto WATTCPCFGERR
 
-REM -- Check if the mTCP config file exists
+:: -- Check if the mTCP config file exists
 IF NOT EXIST %MTCPCFG% goto INPUTERR
 
-REM -- Determine if we're writing the new config
+:: -- Determine if we're writing the new config
 IF "%1"=="/W" goto WRITE
-REM -- Otherwise we parse the mTCP config
+:: -- Otherwise we parse the mTCP config
 goto PARSE-MTCP-CFG
-REM ---------- CHECK ENVIRONMENT ----------
+:: ---------- CHECK ENVIRONMENT ----------
 
-REM ---------- WRITE ----------
-REM -- Convert mTCP config vars to WatTCP config vars
+:: ---------- WRITE ----------
+:: -- Convert mTCP config vars to WatTCP config vars
 :WRITE
 IF "%2"=="IPADDR" echo my_ip=%3>>%WATTCP.CFG%\wattcp.cfg
 IF "%2"=="NETMASK" echo netmask=%3>>%WATTCP.CFG%\wattcp.cfg
 IF "%2"=="GATEWAY" echo gateway=%3>>%WATTCP.CFG%\wattcp.cfg
 IF "%2"=="NAMESERVER" echo nameserver=%3>>%WATTCP.CFG%\wattcp.cfg
 goto END
-REM ---------- WRITE ----------
+:: ---------- WRITE ----------
 
-REM ---------- PARSE MTCP CFG ----------
+:: ---------- PARSE MTCP CFG ----------
 :PARSE-MTCP-CFG
 
-REM -- Clear out the old junk first
+:: -- Clear out the old junk first
 :CLEAR-OLD-FILES
 echo. >%WATTCP.CFG%\wattcp.cfg
 
@@ -49,9 +49,9 @@ call %0 /W %M2WCFG%>>%WATTCP.CFG%\wattcp.cfg
 find "NAMESERVER" <%MTCPCFG% |set /P M2WCFG=
 call %0 /W %M2WCFG%>>%WATTCP.CFG%\wattcp.cfg
 goto END
-REM ---------- PARSE MTCP CFG ----------
+:: ---------- PARSE MTCP CFG ----------
 
-REM ---------- ERRORS ----------
+:: ---------- ERRORS ----------
 :MTCPCFGERR
 echo MTCPCFG environment variable is not set
 goto END
@@ -63,8 +63,8 @@ goto END
 :INPUTERR
 echo %MTCPCFG% does not exist
 goto END
-REM ---------- ERRORS ----------
+:: ---------- ERRORS ----------
 
-REM ---------- END ----------
+:: ---------- END ----------
 :END
 set M2WCFG=
